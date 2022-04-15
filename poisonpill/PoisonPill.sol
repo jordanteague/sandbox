dfc4e17137fb6cdcf82f69169848b34953c171ac
Jordan Teague <ejteague@gmail.com>
2022-04-15 09:46:41 -0400

    Update PoisonPill.sol

--- poisonpill/PoisonPill.sol
+++ poisonpill/PoisonPill.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.10;
 
-// @author Experimental "poison pill" framework for ERC20 token, preventing a single wallet from holding more than a specified percentage of maxSupply.
+// @author Experimental "poison pill" framework for ERC20 token, preventing a single wallet from holding more than a specified percentage of maxSupply.  Would be most effective if used in conjunction with voting in members, where wallet addresses are tied to known individuals.
 
 import "./ERC20.sol";
 
