7d8e4ca1a66f5bcd40f8696359422cb363073cb1
Jordan Teague <ejteague@gmail.com>
2022-06-11 12:46:01 -0400

    Create SafeTransferLib.sol

---
+++ TrustAccount/SafeTransferLib.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.4;
+
+/// @notice Safe ETH and ERC-20 transfer library that gracefully handles missing return values
+/// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol)
+/// License-Identifier: AGPL-3.0-only
+library SafeTransferLib {
+    /// -----------------------------------------------------------------------
+    /// Errors
+    /// -----------------------------------------------------------------------
+
+    error ETHtransferFailed();
+    error TransferFailed();
+    error TransferFromFailed();
+
+    /// -----------------------------------------------------------------------
+    /// ETH Logic
+    /// -----------------------------------------------------------------------
+
+    function _safeTransferETH(address to, uint256 amount) internal {
+        bool success;
+
+        assembly {
+            // transfer the ETH and store if it succeeded or not
+            success := call(gas(), to, amount, 0, 0, 0, 0)
+        }
+        if (!success) revert ETHtransferFailed();
+    }
+
+    /// -----------------------------------------------------------------------
+    /// ERC-20 Logic
+    /// -----------------------------------------------------------------------
+
+    function _safeTransfer(
+        address token,
+        address to,
+        uint256 amount
+    ) internal {
+        bool success;
+
+        assembly {
+            // we'll write our calldata to this slot below, but restore it later
+            let memPointer := mload(0x40)
+            // write the abi-encoded calldata into memory, beginning with the function selector
+            mstore(0, 0xa9059cbb00000000000000000000000000000000000000000000000000000000)
+            mstore(4, to) // append the 'to' argument
+            mstore(36, amount) // append the 'amount' argument
+
+            success := and(
+                // set success to whether the call reverted, if not we check it either
+                // returned exactly 1 (can't just be non-zero data), or had no return data
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // we use 68 because that's the total length of our calldata (4 + 32 * 2)
+                // - counterintuitively, this call() must be positioned after the or() in the
+                // surrounding and() because and() evaluates its arguments from right to left
+                call(gas(), token, 0, 0, 68, 0, 32)
+            )
+
+            mstore(0x60, 0) // restore the zero slot to zero
+            mstore(0x40, memPointer) // restore the memPointer
+        }
+        if (!success) revert TransferFailed();
+    }
+
+    function _safeTransferFrom(
+        address token,
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        bool success;
+
+        assembly {
+            // we'll write our calldata to this slot below, but restore it later
+            let memPointer := mload(0x40)
+            // write the abi-encoded calldata into memory, beginning with the function selector
+            mstore(0, 0x23b872dd00000000000000000000000000000000000000000000000000000000)
+            mstore(4, from) // append the 'from' argument
+            mstore(36, to) // append the 'to' argument
+            mstore(68, amount) // append the 'amount' argument
+
+            success := and(
+                // set success to whether the call reverted, if not we check it either
+                // returned exactly 1 (can't just be non-zero data), or had no return data
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // we use 100 because that's the total length of our calldata (4 + 32 * 3)
+                // - counterintuitively, this call() must be positioned after the or() in the
+                // surrounding and() because and() evaluates its arguments from right to left
+                call(gas(), token, 0, 0, 100, 0, 32)
+            )
+
+            mstore(0x60, 0) // restore the zero slot to zero
+            mstore(0x40, memPointer) // restore the memPointer
+        }
+        if (!success) revert TransferFromFailed();
+    }
+}
